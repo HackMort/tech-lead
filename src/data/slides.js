96968f84a8b9fdc73ee2f807a0dfa4f6eda49270
Jonathan Juarez <jonathan.juarezwp@gmail.com>
2023-07-11 15:08:43 -0600

    Update Content

---
+++ src/data/slides.js
@@ -0,0 +1,164 @@
+export const slides = [
+  {
+    id: 'what-is-it',
+    title: 'What is It?',
+    description: `
+      <h3 class="text-2xl font-bold mb-4">What exactly is a Tech Lead?</h3>
+      <h3 class="text-xl font-bold">Definition 1:</h3>
+      <p class="mb-4">A Tech Lead is a Software Developer who is responsible for ensuring the quality of the technical output of their team is as high as it can be.</p>
+      <p>
+      <h3 class="text-xl font-bold">Definition 2:</h3>
+      <p class="mb-4">A Tech Lead is a crucial role in technology-focused organizations, responsible for overseeing and guiding the technical aspects of a project or a team.</p>
+      <h3 class="text-xl font-bold">Definition 3:</h3>
+      <p class="mb-4">A Tech Lead is a Software Developer who is responsible for leading a team of developers and ensures the quality of the technical products.</p>
+      <h3 class="text-xl font-bold">Conclusion:</h3>
+      <p>A Tech Lead stablish the technical vission and also work with the team to acomplish the main goal. It's also a common thing that the Tech Lead also acquires some of the skills that you would find on some other different roles such as: Team Lead, Architect, Engineering Manager. The difference is that the Tech Lead does not stop coding, but the amount of time spending writing code it decreases.</p>`,
+    image:
+      'https://media.istockphoto.com/id/1255635032/vector/doodle-sketch-question-marks-set-inside-speech-bubbles.jpg?s=612x612&w=0&k=20&c=MUO3m5gx33HeX68MGzlULDKos_96l3BHd4zMmOmGmUo=',
+    featImg:
+      'https://health.wyo.gov/wp-content/uploads/2017/05/man-with-question-mark.jpg'
+  },
+  {
+    id: 'history',
+    title: 'History',
+    description: `<p class="mb-4">In the past, the Tech Lead role was not so common, but nowadays it is a very common role in the software development industry. The Tech Lead role is a natural evolution of the Software Developer role.</p>
+      <p class="mb-4">There are some reasons why the Tech Lead role is becoming more and more common:</p>
+      <ul class="list-disc list-inside mb-4">
+        <li>Software is becoming more and more complex.</li>
+        <li>Software is becoming more and more important.</li>
+        <li>Software is becoming more and more expensive.</li>
+        <li>Software is becoming more and more critical.</li>
+      </ul>
+      <h3 class="text-xl font-bold mb-4">Why did I decide to become a Tech Lead?</h3>
+      <ul class="list-disc list-disc ml-5 mb-4">
+        <li class="list-disc">More responsabilities without stop coding.</li>
+        <li>Learn new skills.</li>
+        <li>Grow my career.</li>
+        <li>Make a bigger impact.</li>
+      </ul>
+      `,
+    image:
+      'https://historytimetv.com/wp-content/uploads/2022/11/HistoryTimeLogo_OnBlackBackground.png',
+    featImg:
+      'https://www.dundryprimary.co.uk/wp-content/uploads/2021/11/istockphoto-1092170968-612x612-1.jpg'
+  },
+  {
+    id: 'responsabilities',
+    title: 'Responsabilities',
+    description: `<p class="mb-4">Like building a Lego tower, building software requires making decisions from the beginning to the end.</p>
+    <p class="mb-4">The first responsibility of a technical leader is to define the engineering reality: What needs to be built, the general technical direction, an example of the golden standard for productivity and technical excellence, the business and technological context and the time and resource constraints.</p>
+    <h3 class="text-xl font-bold mb-4">Some of the Tech Lead responsabilities are:</h3>
+    <ul class="list-disc ml-5 mb-4">
+      <li>Defining reality.</li>
+      <li>Maintaining the project technical vision clear for everyone, from beginning to end.</li>
+      <li>Keeping in mind most engineering details of a project or system.</li>
+      <li>Answering the technical questions that team or stakeholders have.</li>
+      <li>Helping the team resolve difficult engineering problems.</li>
+      <li>Reviewing the decisions the team makes to ensure consistency and alignment with the vision.</li>
+      <li>Translating engineering realities into simple terms for non-technical stakeholders</li>
+      <li>Allow the team to make progress by staying out of their way and protecting them from distractions.</li>
+      <li>Knowing team members strengths and weaknesses.</li>
+      <li>Mentoring team members.</li>
+      <li>Saying, <strong>"Thank you"</strong> to the team</li>
+    </ul>`,
+    image:
+      'https://www.insperity.com/wp-content/uploads/Employee-Related_To-Dos1200x600-1024x538.png',
+    featImg:
+      'https://m.economictimes.com/thumb/msid-68436559,width-1200,height-900,resizemode-4,imgsize-270775/opportunity-getty.jpg'
+  },
+  {
+    id: 'pros-cons',
+    title: 'Pros & Cons',
+    description: `
+    <h2 class="text-2xl font-bold mb-4">Pros and Cons of being a Tech Lead
+    :</h2>
+    <h3 class="text-xl font-bold mb-4">Pros:</h3>
+    <ul class="list-disc ml-5 mb-4">
+      <li>You become a subject matter expert in the area of your project.</li>
+      <li>You have a complete understanding of how the software system works and how to apply changes into that with minimal risk. You can replicate it to other systems now.</li>
+      <li>You become a good communicator because you're responsible for understanding requirements and explaining technical solutions.</li>
+      <li>You reach some level of competency (it depends on the case) in various areas of software development like: <strong>System Desing, Operations, Quality Engineering & Engineering management.</strong></li>
+      <li>Waiting to have all the data before making any decision.</li>
+      <li>Not being available to the team.</li>
+      <li>Being Misaligned With Executive Management</li>
+      <li>Letting Pride, Ego, and Narcissism Guide Decisions.</li>
+      <li>Not Knowing Your People.</li>
+      <li>Giving a bad example.</li>
+    </ul>
+    <h3 class="text-xl font-bold mb-4">Cons:</h3>
+    <ul class="list-disc ml-5 mb-4">
+      <li>At the end of a workday, you often do not have a feeling of accomplishment. You have generated some new work for your team, resolved some blockers but it does not feel like real work.</li>
+      <li>Not enough coding on larger teams.</li>
+      <li>You're the entry point for your team. You should be able to accept tasks from multiple sources:
+      <ul class="list-disc ml-5 mt-4 mb-4">
+        <li>Your teammates</li>
+        <li>Your management</li>
+        <li>Partner teams</li>
+        <li>Customer support team</li>
+        <li>Other people that have heard about your team</li>
+      </ul>
+      </li>
+      <li>Sometimes it's stressful because it's a lot of responsibility. Eventually, you should learn how to handle all that.</li>
+    </ul>`,
+    image:
+      'https://calmack.com/wp-content/uploads/2019/11/pros-and-cons-career-in-court-reporting-denver-co.png',
+    featImg:
+      'https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2020/01/11134324/pros-and-cons.png'
+  },
+  {
+    id: 'mistakes',
+    title: 'Mistakes',
+    description: `<p class="mb-4">
+      Having a Tech Lead role is not easy, and it's very common to make mistakes. Here are some of most common mitakes a Tech Lead can make:
+    </p>
+    <ul class="list-disc ml-5 mb-4">
+      <li>Forcing decisions on the team.</li>
+      <li>Not listening to the team.</li>
+      <li>Falling behind.</li>
+      <li>Losing the trust of the team.</li>
+      <li>Waiting to have all the data before making any decision.</li>
+      <li>Not being available to the team.</li>
+      <li>Being Misaligned With Executive Management</li>
+      <li>Letting Pride, Ego, and Narcissism Guide Decisions.</li>
+      <li>Not Knowing Your People.</li>
+      <li>Giving a bad example.</li>
+    </ul>`,
+    image:
+      'https://cdn.gloveworx.com/images/9_26_mistakes.2e16d0ba.fill-1600x900.jpg',
+    featImg:
+      'https://englishclassviaskype.com/wp-content/uploads/2019/03/Common-mistakes-in-English-to-avoid.png'
+  },
+  {
+    id: 'key-features',
+    title: 'Key Features',
+    description: `
+    <img src="https://blog.talentoit.org/images/TechLeadCircles-2-1024x769.png" alt="Key Features" class="w-full  mb-4">
+    <ul class="list-disc ml-5 mb-4">
+        <li>Understand that sometimes you need to let others lead.</li>
+        <li>Not a full time codeer but still coding.</li>
+        <li>Defining reality of the project.</li>
+        <li>Viewing the project from a high level perspective.</li>
+        <li>Know how to manage other people skills, abilities and how to get the best out of them.</li>
+        <li>Know how to delegate not only tasks but also responsibilities.</li>
+        <li>Aims for the integrity of the team and the project.</li>
+        <li>Dedicates time to the team.</li>
+        <li>Accepts that sometimes imperfection, messiness and chaos are part of the process.</li>
+        <li>Always looks to deliver the message in the best way possible.</li>
+        <li>Knows about Tech, Product, Business and People.</li>
+      </ul>
+      `,
+    image:
+      'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/image_1_mpS2K9q.width-1600.format-webp.webp',
+    featImg:
+      'https://static.semrush.com/blog/uploads/media/04/5c/045c59a82bed12b0a778c9e6e86c583c/serp-features-guide.svg'
+  },
+  {
+    id: 'q-and-a',
+    title: 'Questions & Answers',
+    description: 'Now is the time to ask questions and get answers.',
+    image:
+      'https://www.searchenginejournal.com/wp-content/uploads/2020/03/how-to-make-compelling-qa-videos-to-build-trust-in-your-brand-5e676591b8606.png',
+    featImg:
+      'https://race.agency/wp-content/uploads/2014/07/Questions-and-answers_public-relations-agency_media-relations.jpg'
+  }
+]
